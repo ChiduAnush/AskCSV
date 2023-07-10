b27c4d3edc3a235dc9f72f5aafdd85878ceb6c05
ChiduAnush <chiduanush1@gmail.com>
2023-07-10 17:21:35 +0530

    make UI to upload csv and ask question

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+import streamlit as st
 
 
 def main():
-    print("hello world!")
+    st.set_page_config(page_title="AskCSV")
+    st.header("Ask you CSV 😪")
+
+    user_csv = st.file_uploader("upload your csv file below ", type="csv")
+
+    if user_csv is not None:
+        user_question = st.text_input("yo, ask me something from the csv")
 
 
 if __name__ == "__main__":
